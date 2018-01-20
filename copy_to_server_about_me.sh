e23b95f7b6e3fd5dfe9860a307480e216923b684
Huongg <32060364+Huongg@users.noreply.github.com>
2018-01-20 13:51:33 +0000

    Went back to the green and blue version

--- copy_to_server_about_me.sh
+++ copy_to_server_about_me.sh
@@ -1,1 +1,6 @@
+
+# Delete all files from server
+rm -r /usr/local/var/www/AboutMe/
+
+#  Copy all files from GIt folder to server folder
 cp -r /Users/huongnguyen/Documents/GitHub/about-me/www/ /usr/local/var/www/AboutMe
